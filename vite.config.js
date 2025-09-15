3b9065b2e1e70a00f9fd2fe0e1cd78634d6cb6a5
DuskAI <contato.duskai@gmail.com>
2025-09-15 01:06:26 -0300

    Adiciona configuração de servidores permitidos no Vite

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,11 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server: {
+    allowedHosts: [
+      'prova-paulista.onrender.com'
+    ]
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
